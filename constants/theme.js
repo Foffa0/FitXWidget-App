595c8182ab826cc12e94e5f71b1b0e9de680e4e5
Foffa <schmuck.christopher@gmail.com>
2024-02-15 20:12:03 +0100

    Initial

---
+++ constants/theme.js
@@ -0,0 +1,23 @@
+const COLORS = {
+    primary: "#312651",
+    secondary: "#444262",
+    tertiary: 'FF7754',
+};
+
+const FONT = {
+    regular: "DMRegular",
+    medium: "DMMedium",
+    bold: "DMBold",
+}
+
+const SIZES = {
+    xSmall: 10,
+    small: 12,
+    medium: 16,
+    large: 20,
+    xLarge: 24,
+    xxLarge: 32,
+};
+
+  
+export { COLORS, FONT, SIZES };
