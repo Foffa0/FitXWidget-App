a11b02ef69d41d2dc3ef5fddade00d8388695d2d
Foffa <schmuck.christopher@gmail.com>
2024-03-24 18:45:34 +0100

    Add Sentry to project

--- app/_layout.js
+++ app/_layout.js
@@ -3,8 +3,26 @@ import { useCallback } from 'react';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import 'expo-dev-client';
+import { useNavigationContainerRef } from 'expo-router';
+import {useEffect} from 'react';
+import * as Sentry from '@sentry/react-native';
 
 //SplashScreen.preventAutoHideAsync();
+
+// Construct a new instrumentation instance. This is needed to communicate between the integration and React
+const routingInstrumentation = new Sentry.ReactNavigationInstrumentation();
+
+Sentry.init({
+  dsn: 'https://4776fde8644175c12b2abd1192c53044@o4506961485955072.ingest.us.sentry.io/4506961487724544',
+  debug: true, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
+  integrations: [
+    new Sentry.ReactNativeTracing({
+      // Pass instrumentation to be used as `routingInstrumentation`
+      routingInstrumentation,
+      // ...
+    }),
+  ],
+});
 
 const Layout = () => {
     /*const [fontsLoaded, fontError] = useFonts({
@@ -23,7 +41,15 @@ const Layout = () => {
         return null;
     }*/
 
+    const ref = useNavigationContainerRef();
+
+    useEffect(() => {
+        if (ref) {
+          routingInstrumentation.registerNavigationContainer(ref);
+        }
+    }, [ref]);
+
     return <Stack /*onLayout={onLayoutRootView}*/ />; 
 }
 
-export default Layout;
+export default Sentry.wrap(Layout);
