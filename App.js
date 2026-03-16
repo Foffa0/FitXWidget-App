8a6dafb6368e8a15030eb8d7ff421012e24f465c
Foffa <schmuck.christopher@gmail.com>
2026-03-16 18:09:31 +0100

    Update sentry integration

--- App.js
+++ App.js
@@ -1,14 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Sentry from '@sentry/react-native';
 
-export default function App() {
+Sentry.init({
+  dsn: 'https://4776fde8644175c12b2abd1192c53044@o4506961485955072.ingest.us.sentry.io/4506961487724544',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Enable Logs
+  enableLogs: true,
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
+
+export default Sentry.wrap(function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+});
 
 const styles = StyleSheet.create({
   container: {
